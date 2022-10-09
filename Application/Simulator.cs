69e00920606ce63e3b79b92614289a561bb8ac2f
Chris Stafford <cstafford@tradestation.com>
2022-10-09 15:53:06 -0400

    getting there... #2

--- Application/Simulator.cs
+++ Application/Simulator.cs
@@ -1,6 +1,7 @@
 using CStafford.Moneytree.Infrastructure;
 using CStafford.Moneytree.Models;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using static CStafford.Moneytree.Application.Computer;
 using static CStafford.Moneytree.Models.Simulation;
 
@@ -34,11 +35,21 @@ public class Simulator
 
         await EnsureCharts(1000);
 
-        var chartIdToNumSimulations = new Dictionary<int, int>();
+        var chartIdToNumSimulations = new Dictionary<Chart, int>();
         var charts = await _dbContext.Charts.ToListAsync();
+
         foreach (var chart in charts)
         {
-            chartIdToNumSimulations.Add(chart.Id, numS
+            chartIdToNumSimulations.Add(chart, await _dbContext.Simulations.CountAsync(x => x.ChartId == chart.Id));
+        }
+
+        _logger.LogInformation("Done. Now running simulations");
+
+        while (true)
+        {
+            var lowestChart = chartIdToNumSimulations.OrderBy(x => x.Value).First().Key;
+            chartIdToNumSimulations[lowestChart]++;
+            await RunSimulation(lowestChart);
         }
     }
 
@@ -61,110 +72,111 @@ public class Simulator
         }
     }
 
-    private async Task RunSimulation(int chartId)
+    private async Task RunSimulation(Chart chart)
     {
+        _logger.LogInformation("Running simulation for {chart}", chart);
+        var simulation = new Simulation();
 
-            var simulation = new Simulation();
+        simulation.DepositFrequency = (DepositFrequencyEnum)_random.Next(0, 3);
+        
+        var lowerRange = _earliestDate.Add(TimeSpan.FromDays(chart.DaysSymbolsMustExist));
+        var availableMinutes = _latestDate.Subtract(lowerRange).TotalMinutes;
 
-            simulation.DepositFrequency = (DepositFrequencyEnum)_random.Next(0, 3);
-            
-            var lowerRange = _earliestDate.Add(TimeSpan.FromDays(chart.DaysSymbolsMustExist));
-            var availableMinutes = _latestDate.Subtract(lowerRange).TotalMinutes;
+        simulation.SimulationStart = lowerRange.AddMinutes(_random.Next(0, (int)availableMinutes));
+        availableMinutes = _latestDate.Subtract(simulation.SimulationStart).TotalMinutes;
+        simulation.SimulationEnd = simulation.SimulationStart.AddMinutes(_random.Next(0, (int)availableMinutes));
+        simulation.RunTimeStart = DateTime.Now;
+        simulation.ChartId = chart.Id;
+        
+        var cashDeposited = 0m;
+        var cashOnHand = 0m;
+        var assets = new List<(string symbol, decimal usdPurchasePrice, decimal quanityOwned)>();
 
-            simulation.SimulationStart = lowerRange.AddMinutes(_random.Next(0, (int)availableMinutes));
-            availableMinutes = _latestDate.Subtract(simulation.SimulationStart).TotalMinutes;
-            simulation.SimulationEnd = simulation.SimulationStart.AddMinutes(_random.Next(0, (int)availableMinutes));
-            simulation.RunTimeStart = DateTime.Now;
-            simulation.ChartId = chart.Id;
-            
-            var cashDeposited = 0m;
-            var cashOnHand = 0m;
-            var assets = new List<(string symbol, decimal usdPurchasePrice, decimal quanityOwned)>();
+        var computerContext = new ComputerContext();
+        await computerContext.Init(_dbContext, chart, simulation.SimulationStart);
 
-            var current = simulation.SimulationStart;
-            var nextDeposit = current;
+        var nextDeposit = computerContext.EvaluationTime;
 
-            while (current <= simulation.SimulationEnd)
+        while (computerContext.EvaluationTime <= simulation.SimulationEnd)
+        {
+            if (computerContext.EvaluationTime == nextDeposit)
             {
-                if (current == nextDeposit)
+                cashDeposited += 100m;
+                cashOnHand += 100m;
+
+                switch(simulation.DepositFrequency)
                 {
-                    cashDeposited += 100m;
-                    cashOnHand += 100m;
-
-                    switch(simulation.DepositFrequency)
-                    {
-                        case DepositFrequencyEnum.Monthly:
-                            nextDeposit = nextDeposit.AddMonths(1);
-                            break;
-                        case DepositFrequencyEnum.Weekly:
-                            nextDeposit = nextDeposit.AddDays(7);
-                            break;
-                        case DepositFrequencyEnum.Daily:
-                            nextDeposit = nextDeposit.AddDays(1);
-                            break;
-                    }
+                    case DepositFrequencyEnum.Monthly:
+                        nextDeposit = nextDeposit.AddMonths(1);
+                        break;
+                    case DepositFrequencyEnum.Weekly:
+                        nextDeposit = nextDeposit.AddDays(7);
+                        break;
+                    case DepositFrequencyEnum.Daily:
+                        nextDeposit = nextDeposit.AddDays(1);
+                        break;
                 }
-                
-                var actionsToTake = await _computer.EvaluateMarket(
-                    chart,
-                    cashOnHand > 0m,
-                    assets.Select(x => (x.symbol, x.usdPurchasePrice)).ToList(),
-                    current);
-                
-                foreach (var actionToTake in actionsToTake)
+            }
+            
+            var actionsToTake = await _computer.EvaluateMarket(
+                chart,
+                cashOnHand > 0m,
+                assets.Select(x => (x.symbol, x.usdPurchasePrice)).ToList(),
+                computerContext);
+            
+            foreach (var actionToTake in actionsToTake)
+            {
+                switch (actionToTake.action)
                 {
-                    switch (actionToTake.action)
-                    {
-                        case ActionToTake.Buy:
-                            if (! assets.Any(x => x.symbol == actionToTake.relevantSymbol))
-                            {
-                                var qtyToBuy = cashOnHand / actionToTake.symbolUsdValue.Value;
-                                assets.Add((actionToTake.relevantSymbol, actionToTake.symbolUsdValue.Value, qtyToBuy));
-                            }
-                            else
-                            {
-                                var existing = assets.First(x => x.symbol == actionToTake.relevantSymbol);
-                                assets.Remove(existing);
+                    case ActionToTake.Buy:
+                        if (! assets.Any(x => x.symbol == actionToTake.relevantSymbol))
+                        {
+                            var qtyToBuy = cashOnHand / actionToTake.symbolUsdValue.Value;
+                            assets.Add((actionToTake.relevantSymbol, actionToTake.symbolUsdValue.Value, qtyToBuy));
+                        }
+                        else
+                        {
+                            var existing = assets.First(x => x.symbol == actionToTake.relevantSymbol);
+                            assets.Remove(existing);
 
-                                // calculate a viable usdPurchasePrice porportional to what's owned and what's being bought now
-                                var qtyToBuy = cashOnHand / actionToTake.symbolUsdValue.Value;
-                                var totalQty = qtyToBuy + existing.quanityOwned;
-                                var portionOriginalPrice = existing.quanityOwned / totalQty;
-                                var portionNewPrice = qtyToBuy / totalQty;
-                                var mediatedBuyPrice = (portionOriginalPrice * existing.quanityOwned) + (portionNewPrice * qtyToBuy);
+                            // calculate a viable usdPurchasePrice porportional to what's owned and what's being bought now
+                            var qtyToBuy = cashOnHand / actionToTake.symbolUsdValue.Value;
+                            var totalQty = qtyToBuy + existing.quanityOwned;
+                            var portionOriginalPrice = existing.quanityOwned / totalQty;
+                            var portionNewPrice = qtyToBuy / totalQty;
+                            var mediatedBuyPrice = (portionOriginalPrice * existing.quanityOwned) + (portionNewPrice * qtyToBuy);
 
-                                assets.Add((actionToTake.relevantSymbol, mediatedBuyPrice, totalQty));
-                            }
+                            assets.Add((actionToTake.relevantSymbol, mediatedBuyPrice, totalQty));
+                        }
 
-                            cashOnHand = 0m;
-                            break;
-                        case ActionToTake.Sell:
-                            var asset = assets.First(x => x.symbol == actionToTake.relevantSymbol);
-                            cashOnHand += actionToTake.symbolUsdValue.Value * asset.quanityOwned;
-                            assets.Remove(asset);
-                            break;
-                        case ActionToTake.Hold:
-                            break;
-                    }
+                        cashOnHand = 0m;
+                        break;
+                    case ActionToTake.Sell:
+                        var asset = assets.First(x => x.symbol == actionToTake.relevantSymbol);
+                        cashOnHand += actionToTake.symbolUsdValue.Value * asset.quanityOwned;
+                        assets.Remove(asset);
+                        break;
+                    case ActionToTake.Hold:
+                        break;
                 }
-
-                current = current.AddMinutes(1);
             }
 
-            simulation.RunTimeEnd = DateTime.Now;
-            foreach (var asset in assets)
-            {
-                cashOnHand += asset.quanityOwned + await _computer.MarketValue(asset.symbol, current);
-            }
-            var gain = (cashOnHand - cashDeposited) / cashDeposited;
-            simulation.ResultGainPercentage = gain;
-            await _dbContext.Insert(simulation);
-
-            _logger.LogInformation($"---------------\nRun {i + 1} of 1000");
-            _logger.LogInformation("\n---------------CHART:\n---------------");
-            _logger.LogInformation(chart.ToString());
-            _logger.LogInformation("\n---------------Simulation:\n---------------");
-            _logger.LogInformation(simulation.ToString());
+            await computerContext.NextTick();
         }
+
+        simulation.RunTimeEnd = DateTime.Now;
+
+        foreach (var asset in assets)
+        {
+            cashOnHand += asset.quanityOwned + await _computer.MarketValue(asset.symbol, computerContext.EvaluationTime);
+        }
+
+        var gain = (cashOnHand - cashDeposited) / cashDeposited;
+        simulation.ResultGainPercentage = gain;
+
+        await _dbContext.Insert(simulation);
+
+        _logger.LogInformation("\n---------------Simulation:\n---------------");
+        _logger.LogInformation(simulation.ToString());
     }
 }
